9adb4ce3f0445880c2565effe5ff1e776b7eb2c2
holter21 <hardholter21@gmail.com>
2017-02-22 23:01:12 +0700

    Добавил интерфейс IFigure

--- GeometricsFigure/GeometricsFigure/IFigure.cs
+++ GeometricsFigure/GeometricsFigure/IFigure.cs
@@ -1,7 +1,27 @@
-﻿namespace GeometricsFigure
+﻿using System.Drawing;
+
+namespace GeometricsFigure
 {
+    /// <summary>
+    /// Интерфейс геометрической фигуры.
+    /// </summary>
     public interface IFigure
     {
+        /// <summary>
+        /// Начальные координаты фигуры.
+        /// </summary>
+        Point StartPoint { get; set; }
 
+        /// <summary>
+        /// Получение площади фигуры.
+        /// </summary>
+        /// <returns>Площадь фигуры.</returns>
+        double GetArea();
+
+        /// <summary>
+        /// Получение периметра фигуры.
+        /// </summary>
+        /// <returns>Периметр фигуры.</returns>
+        double GetPerimeter();
     }
 }
