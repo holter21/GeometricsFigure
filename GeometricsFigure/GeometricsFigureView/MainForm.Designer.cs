3671059a118d34e28b219c79b7455159acb4ba77
holter21 <hardholter21@gmail.com>
2017-03-24 19:37:31 +0700

    Добавил MainForm

---
+++ GeometricsFigure/GeometricsFigureView/MainForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GeometricsFigureView
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iFigureBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.startPointDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.areaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perimeterDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddFigureButton = new System.Windows.Forms.Button();
+            this.ModifyFigureButton = new System.Windows.Forms.Button();
+            this.RemoveFigureButton = new System.Windows.Forms.Button();
+            this.RandomFigureButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iFigureBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(681, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.startPointDataGridViewTextBoxColumn,
+            this.areaDataGridViewTextBoxColumn,
+            this.perimeterDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.iFigureBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 266);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // iFigureBindingSource
+            // 
+            this.iFigureBindingSource.DataSource = typeof(GeometricsFigure.IFigure);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.RandomFigureButton);
+            this.groupBox1.Controls.Add(this.RemoveFigureButton);
+            this.groupBox1.Controls.Add(this.ModifyFigureButton);
+            this.groupBox1.Controls.Add(this.AddFigureButton);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(657, 439);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // startPointDataGridViewTextBoxColumn
+            // 
+            this.startPointDataGridViewTextBoxColumn.DataPropertyName = "StartPoint";
+            this.startPointDataGridViewTextBoxColumn.HeaderText = "Координаты Х и Y";
+            this.startPointDataGridViewTextBoxColumn.Name = "startPointDataGridViewTextBoxColumn";
+            this.startPointDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // areaDataGridViewTextBoxColumn
+            // 
+            this.areaDataGridViewTextBoxColumn.DataPropertyName = "Area";
+            this.areaDataGridViewTextBoxColumn.HeaderText = "Площадь";
+            this.areaDataGridViewTextBoxColumn.Name = "areaDataGridViewTextBoxColumn";
+            this.areaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // perimeterDataGridViewTextBoxColumn
+            // 
+            this.perimeterDataGridViewTextBoxColumn.DataPropertyName = "Perimeter";
+            this.perimeterDataGridViewTextBoxColumn.HeaderText = "Периметр";
+            this.perimeterDataGridViewTextBoxColumn.Name = "perimeterDataGridViewTextBoxColumn";
+            this.perimeterDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // AddFigureButton
+            // 
+            this.AddFigureButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.AddFigureButton.Location = new System.Drawing.Point(52, 335);
+            this.AddFigureButton.Name = "AddFigureButton";
+            this.AddFigureButton.Size = new System.Drawing.Size(75, 39);
+            this.AddFigureButton.TabIndex = 2;
+            this.AddFigureButton.Text = "Добавить фигуру";
+            this.AddFigureButton.UseVisualStyleBackColor = true;
+            // 
+            // ModifyFigureButton
+            // 
+            this.ModifyFigureButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ModifyFigureButton.Location = new System.Drawing.Point(210, 335);
+            this.ModifyFigureButton.Name = "ModifyFigureButton";
+            this.ModifyFigureButton.Size = new System.Drawing.Size(75, 39);
+            this.ModifyFigureButton.TabIndex = 3;
+            this.ModifyFigureButton.Text = "Изменить фигуру";
+            this.ModifyFigureButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveFigureButton
+            // 
+            this.RemoveFigureButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.RemoveFigureButton.Location = new System.Drawing.Point(384, 335);
+            this.RemoveFigureButton.Name = "RemoveFigureButton";
+            this.RemoveFigureButton.Size = new System.Drawing.Size(75, 39);
+            this.RemoveFigureButton.TabIndex = 4;
+            this.RemoveFigureButton.Text = "Удалить фигуру";
+            this.RemoveFigureButton.UseVisualStyleBackColor = true;
+            // 
+            // RandomFigureButton
+            // 
+            this.RandomFigureButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.RandomFigureButton.Location = new System.Drawing.Point(539, 335);
+            this.RandomFigureButton.Name = "RandomFigureButton";
+            this.RandomFigureButton.Size = new System.Drawing.Size(75, 39);
+            this.RandomFigureButton.TabIndex = 5;
+            this.RandomFigureButton.Text = "Рандомные фигуры";
+            this.RandomFigureButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 478);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Геометрические фигуры";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iFigureBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource iFigureBindingSource;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startPointDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn areaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perimeterDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button RandomFigureButton;
+        private System.Windows.Forms.Button RemoveFigureButton;
+        private System.Windows.Forms.Button ModifyFigureButton;
+        private System.Windows.Forms.Button AddFigureButton;
+    }
+}
+
