478809b31d96379a6f1fedf3d9d8edb6be547bc6
holter21 <hardholter21@gmail.com>
2017-05-03 01:43:59 +0700

    Format Selection

--- GeometricsFigure/UnitTest/GeometricsFigureModel/CircleTest.cs
+++ GeometricsFigure/UnitTest/GeometricsFigureModel/CircleTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+
 using GeometricsFigure;
+
 using NUnit.Framework;
 
 namespace UnitTest.GeometricsFigureModel
@@ -9,12 +11,13 @@ namespace UnitTest.GeometricsFigureModel
     public class CircleTest
     {
         [Test]
-        [TestCase(3,ExpectedResult = 28.27, TestName = "Тестирование класса CircleFigure свойства Area при верном значении")]
-        [TestCase(-3, ExpectedException = (typeof(ArgumentException)),TestName = "Тестирование класса CircleFigure свойства Area при ошибочном значении, отрицательное значение")]
+        [TestCase(3, ExpectedResult = 28.27, TestName = "Тестирование класса CircleFigure свойства Area при верном значении")]
+        [TestCase(-3, ExpectedException = (typeof(ArgumentException)),
+            TestName = "Тестирование класса CircleFigure свойства Area при ошибочном значении, отрицательное значение")]
         [TestCase(1, ExpectedResult = 3.14, TestName = "Тестирование класса CircleFigure свойства Area при верном значении")]
-        [TestCase(-1, ExpectedException = (typeof(ArgumentException)), TestName = "Тестирование класса CircleFigure свойства Area при ошибочном значении, отрицательное значение")]
+        [TestCase(-1, ExpectedException = (typeof(ArgumentException)),
+            TestName = "Тестирование класса CircleFigure свойства Area при ошибочном значении, отрицательное значение")]
         [TestCase(2, ExpectedResult = 12.57, TestName = "Тестирование класса CircleFigure свойства Area при верном значении")]
-
         public double CircleAreaTest(double radius)
         {
             var circle = new CircleFigure(radius);
@@ -23,11 +26,12 @@ namespace UnitTest.GeometricsFigureModel
 
         [Test]
         [TestCase(3, ExpectedResult = 18.85, TestName = "Тестирование класса CircleFigure свойства Perimeter при верном значении")]
-        [TestCase(-3, ExpectedException = (typeof(ArgumentException)), TestName = "Тестирование класса CircleFigure свойства Perimeter при ошибочном значении, отрицательное значение")]
+        [TestCase(-3, ExpectedException = (typeof(ArgumentException)),
+            TestName = "Тестирование класса CircleFigure свойства Perimeter при ошибочном значении, отрицательное значение")]
         [TestCase(1, ExpectedResult = 6.28, TestName = "Тестирование класса CircleFigure свойства Perimeter при верном значении")]
-        [TestCase(-1, ExpectedException = (typeof(ArgumentException)), TestName = "Тестирование класса CircleFigure свойства Perimeter при ошибочном значении, отрицательное значение")]
+        [TestCase(-1, ExpectedException = (typeof(ArgumentException)),
+            TestName = "Тестирование класса CircleFigure свойства Perimeter при ошибочном значении, отрицательное значение")]
         [TestCase(2, ExpectedResult = 12.57, TestName = "Тестирование класса CircleFigure свойства Perimeter при верном значении")]
-
         public double CirclePerimeterTest(double radius)
         {
             var circle = new CircleFigure(radius);
